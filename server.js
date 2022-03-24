5791ef0c1d37312c815935d9db6583db69b846d4
Huy Phong <phong.bnh@globalfactories.com>
2022-03-24 17:14:57 +0700

    init project

---
+++ server.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+require('dotenv').config();
+
+const typeDefs = gql`
+    type Query {
+        sayHi: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hi world !'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req }),
+})
+
+server.listen(process.env.PORT).then(res => console.log(`Server is running at ${res.url}`)).catch(err => console.error(err));
